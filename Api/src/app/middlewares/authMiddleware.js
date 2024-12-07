b5eb7aff58c40fe234e4ac6af9949d65404e8bcf
Geovane <geovanestuski2@gmail.com>
2024-12-06 21:26:47 -0300

    'c'

--- Api/src/app/middlewares/authMiddleware.js
+++ Api/src/app/middlewares/authMiddleware.js
@@ -5,6 +5,8 @@ const secretKey = process.env.SECRET_KEY;
 module.exports = function authMiddleware(req, res, next) {
   const token = req.headers.authorization?.slice(7);
 
+  console.log({token});
+
   if(!token) {
     return res.status(400).json('Missing access token');
   }
